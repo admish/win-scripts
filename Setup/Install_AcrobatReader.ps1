10b3ff0c0b67205cc58fc20c683e1be917743928
Adm Chrysler <394963+admish@users.noreply.github.com>
2025-12-15 15:20:22 -0500

    Refactor Acrobat Reader installation script
    Updated the script to scrape the latest version from a new URL and adjusted the regex parsing for version extraction.

--- Setup/Install_AcrobatReader.ps1
+++ Setup/Install_AcrobatReader.ps1
@@ -1,23 +1,20 @@
 #Requires -version 5
 
 ## Scrape Adobe's release notes for the latest Windows release
-$ReleaseURI = curl.exe -s https://helpx.adobe.com/acrobat/release-note/release-notes-acrobat-reader.html
-$RegexSyntax = [regex]::new('<a href="(https://www\.adobe\.com/devnet-docs/acrobatetk/tools/ReleaseNotesDC/[^"]+)"[^>]*>(DC [^<]+)</a>', [System.Text.RegularExpressions.RegexOptions]::IgnoreCase)
-$Version = $RegexSyntax.Match($ReleaseURI).Groups[2].Value | cut -d "(" -f2 | cut -d ")" -f1 | sed 's/\.//g'
+$ReleaseURI = curl.exe -s https://www.adobe.com/devnet-docs/acrobatetk/tools/ReleaseNotesDC/index.html
+
+## Parse $ReleaseURI to find the latest version
+$ReleaseURI -match '\b\d{2}\.\d{3}\.\d{5}\b' 
+
+## Format version to build static download URL
+$Version = $matches[0] | cut -d "(" -f2 | cut -d ")" -f1 | sed 's/\.//g'
+$URI = "https://ardownload2.adobe.com/pub/adobe/acrobat/win/AcrobatDC/" + $Version + "/AcroRdrDCx64" + $Version + "_en_US.exe"
 
 ## Set local enviroment
 $Path = "$env:TEMP\"
 $Installer = "Acrobat_Reader.exe"
-$URI = "https://ardownload2.adobe.com/pub/adobe/acrobat/win/AcrobatDC/" + $Version + "/AcroRdrDCx64" + $Version + "_en_US.exe"
 
+## Download and install latest version of Adobe Acrobat Reader
 $wc = New-Object System.Net.WebClient; $wc.DownloadFile("$URI", "$Path$Installer")
 Start-Process -FilePath $Path$Installer -Args "/sAll /rs /rps /msi /norestart /quiet EULA_ACCEPT=YES" -WorkingDirectory $Path -Wait
 Remove-Item $Path$Installer
-
-
-
-####
-## The following urls are exclusively for the macOS version, which doesn't always correlate with Windows releases
-
-# $ReaderVersion = irm "https://armmf.adobe.com/arm-manifests/mac/AcrobatDC/reader/current_version.txt" 
-# $AcrobatVersion = irm "https://armmf.adobe.com/arm-manifests/mac/AcrobatDC/acrobat/current_version.txt" 
