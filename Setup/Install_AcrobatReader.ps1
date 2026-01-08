9d43c0a2bf94f949bb4fec677f903cd1dbd3c960
Adm Chrysler <394963+admish@users.noreply.github.com>
2026-01-08 15:43:58 -0500

    Refactor Acrobat Reader installation script

--- Setup/Install_AcrobatReader.ps1
+++ Setup/Install_AcrobatReader.ps1
@@ -1,20 +1,21 @@
 #Requires -version 5
 
 ## Scrape Adobe's release notes for the latest Windows release
-$ReleaseURI = curl.exe -s https://www.adobe.com/devnet-docs/acrobatetk/tools/ReleaseNotesDC/index.html
+$ReleaseURI = curl.exe -LSs https://www.adobe.com/devnet-docs/acrobatetk/tools/ReleaseNotesDC/index.html
 
 ## Parse $ReleaseURI to find the latest version
-$ReleaseURI -match '\b\d{2}\.\d{3}\.\d{5}\b' 
+$RegexSyntax = [regex]::new('\b\d{2}\.\d{3}\.\d{5}\b' , [System.Text.RegularExpressions.RegexOptions]::IgnoreCase)
 
 ## Format version to build static download URL
-$Version = $matches[0] | cut -d "(" -f2 | cut -d ")" -f1 | sed 's/\.//g'
+$Version = $RegexSyntax.Match($ReleaseURI).Value | cut -d "(" -f2 | cut -d ")" -f1 | sed 's/\.//g'
 $URI = "https://ardownload2.adobe.com/pub/adobe/acrobat/win/AcrobatDC/" + $Version + "/AcroRdrDCx64" + $Version + "_en_US.exe"
 
 ## Set local enviroment
 $Path = "$env:TEMP\"
-$Installer = "Acrobat_Reader.exe"
+$Installer = "/AcroRdrDCx64" + $Version + "_en_US.exe"
 
 ## Download and install latest version of Adobe Acrobat Reader
 $wc = New-Object System.Net.WebClient; $wc.DownloadFile("$URI", "$Path$Installer")
 Start-Process -FilePath $Path$Installer -Args "/sAll /rs /rps /msi /norestart /quiet EULA_ACCEPT=YES" -WorkingDirectory $Path -Wait
 Remove-Item $Path$Installer
+
