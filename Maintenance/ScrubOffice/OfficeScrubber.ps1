a84130a1ec62473395facae4aa54fb30503ea190
Adm Chrysler <adm@p1tech.net>
2026-01-20 11:23:40 -0500

    Updated OfficeScrubber

--- Maintenance/ScrubOffice/OfficeScrubber.ps1
+++ Maintenance/ScrubOffice/OfficeScrubber.ps1
@@ -1,9 +1,21 @@
 ## Downloads and runs script that will fully clean any & all traces of installed versions of Office
-## https://github.com/abbodi1406/BatUtil/tree/master/OfficeScrubber
-## https://github.com/abbodi1406/WHD/raw/refs/heads/master/scripts/OfficeScrubber_13.zip
 
-Invoke-WebRequest -Uri "https://github.com/abbodi1406/WHD/raw/refs/heads/master/scripts/OfficeScrubber_13.zip" -OutFile "C:\Windows\Temp\OfficeScrubber_13.zip"
+## Original source:
+# https://officedev.github.io/
+# https://github.com/OfficeDev/Office-IT-Pro-Deployment-Scripts/
 
-Expand-Archive -Path "C:\Windows\Temp\OfficeScrubber_13.zip" -DestinationPath "C:\Windows\Temp\OfficeScrubber" -Force
+## Alternate mirrors
+# https://web.archive.org/web/20251209063426/https://github.com/OfficeDev/Office-IT-Pro-Deployment-Scripts/
+# https://github.com/abbodi1406/BatUtil/tree/master/OfficeScrubber
+# https://github.com/abbodi1406/WHD/blob/master/scripts/OfficeScrubber_14.zip
 
-cmd /c "C:\Windows\Temp\OfficeScrubber\OfficeScrubber.cmd /A"
+
+$workPath = "$env:TEMP"
+$downFile = "\OfficeScrubber.zip"
+$sourceURI = "https://github.com/admish/win-scripts/blob/main/Maintenance/ScrubOffice/OfficeScrubber.zip"
+
+$wc = New-Object System.Net.WebClient; $wc.DownloadFile("$sourceURI", "$workPath$downFile")
+
+Expand-Archive -Path $workPath$downFile -DestinationPath "$workPath\ScrubOffice" -Force
+
+cmd /c "$workPath\ScrubOffice\OfficeScrubber.cmd /A"
