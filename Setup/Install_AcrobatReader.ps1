3cd8fa3ab2bfe41e2650b8797f02f53bf6e1fb00
Adm Chrysler <394963+admish@users.noreply.github.com>
2026-01-20 11:57:19 -0500

    Update Install_AcrobatReader.ps1

--- Setup/Install_AcrobatReader.ps1
+++ Setup/Install_AcrobatReader.ps1
@@ -7,7 +7,7 @@ $ReleaseURI = curl.exe -LSs https://www.adobe.com/devnet-docs/acrobatetk/tools/R
 $RegexSyntax = [regex]::new('\b\d{2}\.\d{3}\.\d{5}\b' , [System.Text.RegularExpressions.RegexOptions]::IgnoreCase)
 
 ## Format version to build static download URL
-$Version = $RegexSyntax.Match($ReleaseURI).Value | cut -d "(" -f2 | cut -d ")" -f1 | sed 's/\.//g'
+$Version = $RegexSyntax.Match($ReleaseURI).Value.replace(".","")
 $URI = "https://ardownload2.adobe.com/pub/adobe/acrobat/win/AcrobatDC/" + $Version + "/AcroRdrDCx64" + $Version + "_en_US.exe"
 
 ## Set local enviroment
@@ -18,4 +18,5 @@ $Installer = "/AcroRdrDCx64" + $Version + "_en_US.exe"
 $wc = New-Object System.Net.WebClient; $wc.DownloadFile("$URI", "$Path$Installer")
 Start-Process -FilePath $Path$Installer -Args "/sAll /rs /rps /msi /norestart /quiet EULA_ACCEPT=YES" -WorkingDirectory $Path -Wait
 Remove-Item $Path$Installer
+
 
