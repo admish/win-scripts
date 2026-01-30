7f1d307e10b43a2d48aaeba5c6ffd4cdb7288340
Adm Chrysler <394963+admish@users.noreply.github.com>
2026-01-30 14:22:33 -0500

    Update Dell Command Update download links and version

--- Setup/Install_DellCommand.ps1
+++ Setup/Install_DellCommand.ps1
@@ -1,22 +1,12 @@
 #Requires -version 5.1
 #Requires -RunAsAdministrator
 
-## KB Doc: https://www.dell.com/support/kbdoc/en-us/000177325/dell-command-update
+## Documentation to locate most recent version: https://www.dell.com/support/kbdoc/en-us/000177325/dell-command-update
 
-## Source: https://www.dell.com/support/home/en-us/drivers/driversdetails?driverid=p83k5
-$DCU530 = "https://downloads.dell.com/FOLDER11563484M/1/Dell-Command-Update-Windows-Universal-Application_P83K5_WIN_5.3.0_A00.EXE"
-## Source: https://www.dell.com/support/home/en-us/drivers/driversdetails?driverid=9m35m
-$DCU540 = "https://dl.dell.com/FOLDER11914128M/1/Dell-Command-Update-Windows-Universal-Application_9M35M_WIN_5.4.0_A00.EXE"
-## Source: https://www.dell.com/support/home/en-us/drivers/driversdetails?driverid=c8jxv
-$DCU550 = "https://dl.dell.com/FOLDER13309588M/2/Dell-Command-Update-Windows-Universal-Application_C8JXV_WIN64_5.5.0_A00_01.EXE"
+$DCU = "https://dl.dell.com/FOLDER13922692M/1/Dell-Command-Update-Windows-Universal-Application_2WT0J_WIN64_5.6.0_A00.EXE"
 
+$dPath = "$env:TEMP\"
+$Installer = "Dell-Command-Update_5.6.0_A00.exe"
 
-$dPath = "$env:SystemDrive\Support\"
-$Installer = "\Dell-Command-Update_5.5.0.exe"
-if(!(Test-Path -PathType Container $dPath))
-{
-      New-Item -ItemType Directory -Path $dPath
-}
-
-$wc = New-Object System.Net.WebClient; $wc.DownloadFile("$DCU550", "$dPath$Installer")
+$wc = New-Object System.Net.WebClient; $wc.DownloadFile("$DCU", "$dPath$Installer")
 Start-Process -FilePath $dPath$Installer -ArgumentList '/s' -Wait -NoNewWindow -ErrorAction Stop
